c681cf8c773e52d255c5b271eb6cab0cba785448
seungbum94 <tmdqja5527@gmail.com>
2018-10-11 22:28:07 +0900

    upload

--- WegisHardware/cam/pircam.py
+++ WegisHardware/cam/pircam.py
@@ -13,16 +13,17 @@ setup = 0
 while True:
     input_state = GPIO.input(21)
     if input_state == True:
-	print("Motion Detected")
+	print("Human Detect")
 	time.sleep(2)
 	
 	if setup == 0:
-            s = "%04d_%02d_%02d_%02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
+            s = "%04d/%02d/%02d_%02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
 #now.tm_year
 
             name = s + ".h264"
             name2 = s + ".mp4"
 
+	    print "Camera ON"
     	    camera = picamera.PiCamera()
     	    camera.resolution = (640, 480)
     	    camera.start_recording("1.h264") # name...
@@ -33,10 +34,12 @@ while True:
 	i = i+1
 	if i == 3:
 	    camera.stop_recording()
+	    print "Camera OFF"
 	    call = "MP4Box -add" + " " + name + " " + name2
- 	    print call
+ 	    #print call
  	    call2 = "MP4Box -add" + " " + "1.h264" + " " + name2
   	    subprocess.call(call2, shell=True)
   	    subprocess.call("rm 1.h264 ",shell=True)
+	    print "sucess"
 	    i = 0
 	    
