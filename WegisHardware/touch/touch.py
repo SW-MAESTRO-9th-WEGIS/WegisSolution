b3a797923dbb866da70a2a7a249865f4f445a09c
seungbum94 <tmdqja5527@gmail.com>
2018-11-02 03:34:45 +0900

    pwd upgrade

--- WegisHardware/touch/touch.py
+++ WegisHardware/touch/touch.py
@@ -2,6 +2,7 @@
 
 import RPi.GPIO as GPIO
 import mpr121
+import time
 
 # Use GPIO Interrupt Pin
 
@@ -19,24 +20,33 @@ mpr121.setup(0x5b)
 # Track touches
 
 touches = [0,0,0,0,0,0,0,0,0,0,0,0];
+pwd = [0,0,0,0];
+j = 0
 
 while True:
 
-	if (GPIO.input(7)): # Interupt pin is high
-		pass
-	else: # Interupt pin is low
+    if (GPIO.input(7)): # Interupt pin is high
+	pass
+    else: # Interupt pin is low
 
-		touchData = mpr121.readData(0x5b)
+	touchData = mpr121.readData(0x5b)
 
-		for i in range(12):
-			if (touchData & (1<<i)):
+	for i in range(12):
+	    if (touchData & (1<<i)):
 
-				if (touches[i] == 0):
+		if (touches[i] == 0):
 
-					print( 'Pin ' + str(i) + ' was just touched')
+			print( 'Pin ' + (str(i+1)) + ' was just touched')
+			time.sleep(0.3)
+			touches[i] = 1;
+			pwd[j] = (i+1)
+			j=j+1
+			if j==4 :
+			    j=0
 
-				touches[i] = 1;
-			else:
-				if (touches[i] == 1):
-					print( 'Pin ' + str(i) + ' was just released')
-				touches[i] = 0;
+	    else:
+		if (touches[i] == 1):
+			print( 'Pin ' + str(i+1) + ' was just released')
+			time.sleep(0.3)
+			touches[i] = 0;
+			print pwd
